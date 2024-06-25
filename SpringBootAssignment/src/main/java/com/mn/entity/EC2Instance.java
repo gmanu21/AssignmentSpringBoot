813ef1a2a74bcb7968f4548628695bb813ac065f
Manohar <118408671+gmanu21@users.noreply.github.com>
2024-06-25 12:09:53 +0530

    Update EC2Instance.java

--- SpringBootAssignment/src/main/java/com/mn/entity/EC2Instance.java
+++ SpringBootAssignment/src/main/java/com/mn/entity/EC2Instance.java
@@ -1,8 +1,20 @@
 package com.mn.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class EC2Instance {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
 	private String instanceId;
 
+	public EC2Instance() {
+	
+	}
 	public EC2Instance(String instanceId) {
 		this.instanceId = instanceId;
 	}
