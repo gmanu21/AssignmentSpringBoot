deaca444b384e63a2a28cb02850aa61516ca450b
Manohar <118408671+gmanu21@users.noreply.github.com>
2024-06-25 12:11:14 +0530

    Update EC2InstanceRepository.java

--- SpringBootAssignment/src/main/java/com/mn/repository/EC2InstanceRepository.java
+++ SpringBootAssignment/src/main/java/com/mn/repository/EC2InstanceRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.mn.entity.EC2Instance;
 
-public interface EC2InstanceRepository extends JpaRepository<EC2Instance, String> {
+public interface EC2InstanceRepository extends JpaRepository<EC2Instance, Integer> {
 	
 }
